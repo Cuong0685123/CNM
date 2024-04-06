34e4e3f2d674ac33bb4aeba0217b1069d384ac13
Cuong0685123 <eticon0685@gmail.com>
2024-04-06 10:02:12 +0700

    init

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,15 +1,79 @@
+import User from '../models/user.model.js';
+import bcryptjs from "bcryptjs";
+import generateTokenAndSetCookie from '../utils/generateToken.js';
 export const signup = async (req, res)=> {
     try{
         const {fullName, username, password,confirmPassword,gender } = req.body;
+if(password !== confirmPassword){
+    return res.status(400).json({error:"Mat Khau khong khop"})
+}
+
+
+const user = await User.findOne({username});
+if(user){
+    return res.status(400).json({error:"Ten dang nhap da ton tai"})
+}
+
+const salt = await bcryptjs.genSalt(10);
+const hashedPassword = await bcryptjs.hash(password, salt);
+
+const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
+const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
+
+const newUser = new User({
+    fullName,
+    username,
+    password: hashedPassword,
+    gender,
+    profilePic: gender === "male" ? boyProfilePic : girlProfilePic
+});
+if(newUser){
+await generateTokenAndSetCookie(newUser._id,res);
+await newUser.save();
+res.status(210).json({
+    _id:newUser._id,
+    fullName: newUser.fullName,
+    username: newUser.username,
+    profilePic: newUser.profilePic,
+});
+}else{
+    res.status(400).json({error: "Khong co user"});
+}
     }catch(error){
-        
+        console.log(`loi khong the dang ki`,error.message);
+        res.status(500).json({error:"loi "});
+
     }
 };
 
-export const login = (req, res)=> {
-    console.log("loginUser");
+export const login = async (req, res)=> {
+    try {
+        const {username, password} = req.body;
+        const user = await User.findOne({username});
+        const isPasswordCorrect = await bcryptjs.compare(password, user.password || "");
+
+        if(!user || !isPasswordCorrect){
+return res.status(400).json({error:"sai ten dang nhap hoac mat khau"});
+        }
+        generateTokenAndSetCookie(user._id, res);
+res.status(200).json({
+    _id: user._id,
+    fullName: user.fullName,
+    username: user.username,
+    profilePic: user.profilePic,
+});
+    } catch (error) {
+        console.log(`loi khong the dang nhap`,error.message);
+        res.status(500).json({error:"loi "});
+    }
 };
 
-export const logout = (req, res)=> {
-    console.log("logoutUser");
+export const logout = async (req, res)=> {
+    try {
+        res.cookie("jwt","",{maxAge: 0});
+        res.status(200).json({message:"dang xuat thanh cong"});
+    } catch (error) {
+        console.log("loi dang xuat",error.message);
+        res.status(500).json({error:"loi "});
+    }
 };
--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,29 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import cookieParser from "cookie-parser";
+import path from "path";
 import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
 
-const app = express();
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res)=>{
-    res.send("Hello world");
-});
-
+const __dirname = path.resolve();
+app.use(cookieParser());
+app.use(express.json());
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 app.listen(PORT,()=> {
     connectToMongoDB();
